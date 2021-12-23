9fa39e7ac4d65a2558f1fd6aee426975039fc687
Denys Balazh <73472742+denysitis@users.noreply.github.com>
2021-12-23 20:15:55 +0200

    Add all files

---
+++ variables.tf
@@ -0,0 +1,10 @@
+
+variable "SERVER_USER" {
+    type = string
+    description = "Login for Virtual Machine in Azure"
+}
+
+variable "SERVER_PAASS" {
+    type = string
+    description = "Password for Virtual Machine in Azure"
+}
---
+++ vars.sh
@@ -0,0 +1,2 @@
+export TF_VAR_SERVER_USER="denden"
+export TF_VAR_SERVER_PAASS="Password@.159753" 
